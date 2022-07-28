3956e6c2d028e3839237245523a9db7044a3f87d
José Carlos <jose.carlos@luby.software>
2022-07-28 15:12:56 -0300

    fix: seconds expires oauthtoken

--- auth/server.js
+++ auth/server.js
@@ -5,7 +5,7 @@ const tokenGenerate = require("../helper/token-jwt");
 const oauthModel = require("./oauth-model")(clientDB, tokenDB, tokenGenerate);
 
 const EXPIRES_TOKEN_INSECONDS = process.env.EXPIRES_TOKEN_INSECONDS
-  ? parseInt(process.env.EXPIRES_TOKEN_INSECONDS)
+  ? (parseInt(process.env.EXPIRES_TOKEN_INSECONDS) + 1)
   : 3600;
 module.exports = new OAuthServer({
   model: oauthModel,
